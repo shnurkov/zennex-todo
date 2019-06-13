99bd2b4d9ee0e3b63d3710f55eecf28c0a790c9a
ssa <sema@Mac-mini-Admin.local>
2019-06-13 14:58:14 +0700

    add new task opportunity

--- src/components/tasks-list/task-list.js
+++ src/components/tasks-list/task-list.js
@@ -6,7 +6,9 @@ import Task from "../task/task";
 
 export default class TaskList extends Component{
   state = {
-    tasks
+    tasks,
+    count: 2, //temp
+    newTaskTitle: ""
   }
   render(){
     const taskElements = this.state.tasks.map((task) => {
@@ -20,10 +22,38 @@ export default class TaskList extends Component{
           </ul>
         </div>
         <div className="tasks-list__add-field-wrap">
-          <input className="tasks-list__add-field" placeholder="Type a task" type="text"/>
+          <input className="tasks-list__add-field" placeholder="Type a task" type="text" value = {this.state.newTaskTitle} onChange = {this.handleChange} onKeyPress = {this.handleKeyPress}/>
           <button className="tasks-list__add-field__btn"></button>
         </div>
       </div>
     );
   }
+  handleChange = (e) => {
+    this.setState({newTaskTitle: e.target.value});
+  }
+  handleKeyPress = (e) => {
+    if(e.key === "Enter" && this.state.newTaskTitle){
+      this.setState({newTaskTitle: ""});
+      this.addNewTask();
+    }
+  }
+  addNewTask = () => {
+    let tasks = this.state.tasks;
+    let count = this.state.count;
+    count++;
+    tasks.push({
+      id: count, 
+      title: this.state.newTaskTitle,
+      description: null,
+      importance: null,
+      time: {
+        end: null,
+        done: null
+      },
+      isExpired: false,
+      isDone: false
+    });
+    this.setState({tasks, count});
+  }
+
 }
