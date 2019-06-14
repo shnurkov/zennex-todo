3d165517a97ed40c5f837e77b52c9c87c1be4da9
ssa <sema@Mac-mini-Admin.local>
2019-06-14 12:35:37 +0700

    add task form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./index.scss";
+import 'antd/dist/antd.css';
 import TaskList from "./components/tasks-list/task-list";
 import TaskDesc from "./components/task-description/task-description";
 
--- src/components/task-description/task-description.js
+++ src/components/task-description/task-description.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
+import { Input, Radio, DatePicker, TimePicker, Button } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
 
 import "./style.scss";
+// import moment from 'moment';
 
 export default class TaskDesc extends Component{
   render(){
     return (
       <div className="task-desc">
+        <div className="task-desc__row">
+          <label htmlFor="title">Name</label>
+          <Input id = "title"/>
+        </div>
+        <div className="task-desc__row">
+          <label htmlFor="desc">Description</label>
+          <TextArea id="desc"/>
+        </div>
+        <div className="task-desc__row">
+          <label htmlFor="importance">Importance</label>
+          <Radio.Group defaultValue="usual" buttonStyle="solid">
+            <Radio.Button value="usual">Usual</Radio.Button>
+            <Radio.Button value="important">Important</Radio.Button>
+            <Radio.Button value="very-important">Very Important</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div className="task-desc__row">
+          <label htmlFor="date-end">Date end</label>
+          <DatePicker id="date-end" />
+          <TimePicker format="HH:mm" />
+        </div>
+        {false && 
+        <div className="task-desc__row">
+          <label htmlFor="date-done">Date done</label>
+          <DatePicker id="date-done" />
+          <TimePicker format="HH:mm" />
+        </div>}
+        <div className="task-desc__delete-btn">
+        <Button type="danger" block>
+          Delete
+        </Button>
+        </div>
       </div>
     );
   }
